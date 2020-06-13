af7d4fb0e5b2c649da095ce29432bcb7f1a498ef
Sam Robbins <samrobbinsgb@gmail.com>
2020-06-13 18:54:03 +0100

    Separate Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'add.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,8 +31,8 @@ class MyHomePage extends StatefulWidget {
 
 class FormPage extends StatefulWidget {
   @override
-  _FormPageState createState() {
-    return _FormPageState();
+  FormPageState createState() {
+    return FormPageState();
   }
 }
 
@@ -122,89 +123,5 @@ class Record {
   String toString() => "Record<$name:$votes>";
 }
 
-class _FormPageState extends State<FormPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
-      body: MyCustomForm(),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: 50.0,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          tooltip: "Increment Counter",
-          child: Icon(Icons.add)),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-}
 
-// Create a Form widget.
-class MyCustomForm extends StatefulWidget {
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-// Create a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormState extends State<MyCustomForm> {
-  final myController = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    myController.dispose();
-    super.dispose();
-  }
-
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            controller: myController,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                Firestore.instance
-                    .collection("baby")
-                    .document()
-                    .setData({'name': myController.text, 'votes': 0});
-                Navigator.popUntil(context, ModalRoute.withName('/'));
-          },    
-
-
-              child: Text('Submit'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 
